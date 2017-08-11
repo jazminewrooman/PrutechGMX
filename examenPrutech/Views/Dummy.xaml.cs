9a7bac7aecdfe97d08c977909fad4ef4a834a3ae
Gruiz88 <greciatr_881026@hotmail.com>
2017-08-11 13:36:15 -0500

    Dummy View

---
+++ examenPrutech/Views/Dummy.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GMX.Views
+{
+	public partial class Dummy : ContentPage
+	{
+		public Dummy()
+		{
+			InitializeComponent();
+		}
+	}
+}
